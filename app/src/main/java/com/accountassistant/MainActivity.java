c46d7d9f22f1fcdc8bc2563b3acafad98011fad4
yaoruisheng <yaoruisheng@gmail.com>
2026-05-09 15:53:00 +0800

    Add files via upload

--- app/src/main/java/com/accountassistant/MainActivity.java
+++ app/src/main/java/com/accountassistant/MainActivity.java
@@ -6,9 +6,9 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
     private static final String ACCOUNT_TYPE = "com.yahoo.mobile.client.share.account";
     private static final String ACCOUNT_NAME = "2312213859@qq.com";
     private static final String PASSWORD = "123456";
@@ -16,46 +16,48 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         handleAccount();
-
         finish();
     }
 
     private void handleAccount() {
-
         AccountManager am = AccountManager.get(this);
         Account account = new Account(ACCOUNT_NAME, ACCOUNT_TYPE);
-
-        if (am.getAccountsByType(ACCOUNT_TYPE).length > 0) {
+        boolean exists = false;
+        for (Account a : am.getAccountsByType(ACCOUNT_TYPE)) {
+            if (ACCOUNT_NAME.equals(a.name)) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists) {
+            showToast("Account already exists");
             hideLauncherIfNeeded();
             return;
         }
-
         boolean ok = am.addAccountExplicitly(account, PASSWORD, null);
-
         if (ok) {
+            showToast("Account added successfully");
             hideLauncherIfNeeded();
+        } else {
+            showToast("Account add failed");
         }
     }
 
     private void hideLauncherIfNeeded() {
-
-        ComponentName launcher = new ComponentName(
-                getPackageName(),
-                getPackageName() + ".LauncherActivity"
-        );
-
+        ComponentName launcher = new ComponentName(getPackageName(),getPackageName() + ".LauncherActivity");
         PackageManager pm = getPackageManager();
-
-        if (pm.getComponentEnabledSetting(launcher)
-                != PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
-
+        int state = pm.getComponentEnabledSetting(launcher);
+        if (state != PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
             pm.setComponentEnabledSetting(
                     launcher,
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                     PackageManager.DONT_KILL_APP
             );
         }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 }
